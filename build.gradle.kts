ce89b44f52e6d2534cf472c6439a12df5eacced0
abuhrov <anatolii.buhrov@agrichain.com.ua>
2020-10-07 13:20:19 +0300

    remove heroku plugin and add system.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     application
     kotlin("jvm") version "1.4.10"
-    id("com.heroku.sdk.heroku-gradle") version "2.0.0"
 }
 
 group = "me.abuhrov"
@@ -16,11 +15,6 @@ dependencies {
     implementation("com.github.elbekD:kt-telegram-bot:1.3.5")
     implementation("com.google.apis:google-api-services-sheets:v4-rev20200922-1.30.10")
     implementation("com.google.auth:google-auth-library-oauth2-http:0.21.1")
-}
-
-heroku {
-    isIncludeBuildDir = false
-    jdkVersion = "15"
 }
 
 application {
