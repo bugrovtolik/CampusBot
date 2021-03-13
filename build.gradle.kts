2758dbc75df95dce041addef23a97344f35256dc
abuhrov <anatolii.buhrov@agrichain.com.ua>
2021-03-13 14:50:59 +0200

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.elbekD:kt-telegram-bot:1.3.6")
+    implementation("com.github.elbekD:kt-telegram-bot:1.3.8")
     implementation("com.google.apis:google-api-services-sheets:v4-rev20200922-1.30.10")
     implementation("com.google.auth:google-auth-library-oauth2-http:0.21.1")
 }
